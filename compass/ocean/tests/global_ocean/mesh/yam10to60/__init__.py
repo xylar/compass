7cd4c1966faef6295c54da5cb43e11225f25cf8a
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-05-27 19:02:03 -0600

    TEMP: add a 20 km high res Atlantic region

--- compass/ocean/tests/global_ocean/mesh/yam10to60/__init__.py
+++ compass/ocean/tests/global_ocean/mesh/yam10to60/__init__.py
@@ -1,10 +1,26 @@
 import mpas_tools.mesh.creation.mesh_definition_tools as mdt
 import numpy as np
+from geometric_features import read_feature_collection
+from mpas_tools.cime.constants import constants
+from mpas_tools.mesh.creation.signed_distance import (
+    signed_distance_from_geojson,
+)
 
 from compass.mesh import QuasiUniformSphericalMeshStep
 
 
 class YAM10to60BaseMesh(QuasiUniformSphericalMeshStep):
+    """
+    A step for creating YAM10to60 meshes
+    """
+    def setup(self):
+        """
+        Add some input files
+        """
+        package = 'compass.ocean.tests.global_ocean.mesh.yam10to60'
+        self.add_input_file(filename='northern_south_atlantic.geojson',
+                            package=package)
+        super().setup()
 
     def build_cell_width_lat_lon(self):
         """
@@ -32,5 +48,32 @@ class YAM10to60BaseMesh(QuasiUniformSphericalMeshStep):
 
         cell_width_vs_lat = mdt.EC_CellWidthVsLat(lat)
         cell_width = np.outer(cell_width_vs_lat, np.ones([1, lon.size]))
+
+        # read the shape
+        fc = read_feature_collection('northern_south_atlantic.geojson')
+
+        # How wide in meters the smooth transition between the background
+        #   resolution and the finer resolution regions should be.
+        # 1200 km is equivalent to about 10 degrees latitude
+        trans_width = 1200e3
+
+        # The resolution in km of the finer resolution region
+        fine_cell_width = 20.
+
+        # the radius of the earth defined in E3SM's shared constants
+        earth_radius = constants['SHR_CONST_REARTH']
+
+        # A field defined on the lat-long grid with the signed distance away
+        # from the boundary of the shape (positive outside and negative inside)
+        atlantic_signed_distance = signed_distance_from_geojson(
+            fc, lon, lat, earth_radius, max_length=0.25)
+
+        # A field that goes smoothly from zero inside the shape to one outside
+        # the shape over the given transition width.
+        weights = 0.5 * (1 + np.tanh(atlantic_signed_distance / trans_width))
+
+        # The cell width in km becomes a blend of the background cell width
+        # and the finer cell width using the weights
+        cell_width = fine_cell_width * (1 - weights) + cell_width * weights
 
         return cell_width, lon, lat
